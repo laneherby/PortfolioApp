ec0d49050227a88f79f8026808702fda11dec280
Lane Herberholz <laneherberholz@gmail.com>
2021-03-21 20:43:30 +0000

    Added index file for main components

---
+++ components/main/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import Comments from "./Comments";
+import Contact from "./Contact";
+import PushNotifications from "./PushNotifications";
+import QRScanner from "./QRScanner";
+
+export {
+    Home,
+    Comments,
+    Contact,
+    PushNotifications,
+    QRScanner
+};
