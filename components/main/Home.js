331a34daa6d420217c233c04fcef4573784640eb
Lane Herberholz <laneherberholz@gmail.com>
2021-04-02 18:35:18 +0000

    Added a base theme with primary and accent and background colors

--- components/main/Home.js
+++ components/main/Home.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useTheme, Card } from 'react-native-paper';
+import { color } from 'react-native-reanimated';
 
 const Home = () => {
+  const { colors } = useTheme();
+  //console.log(colors);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Home Page</Text>
+      <Text style={{color: colors.text}}>
+        Home Page
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 
 export default Home;
